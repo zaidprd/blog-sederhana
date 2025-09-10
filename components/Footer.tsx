2ad8d2c4c8a8748ad720fa7fb05e453bc34b774a
zaidprd <160503080+zaidprd@users.noreply.github.com>
2025-09-10 09:34:07 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,8 +27,8 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
+          <Link href="https://optimis.my.id.com/">
+            Optimis
           </Link>
         </div>
       </div>
